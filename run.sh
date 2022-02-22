013899800c394e4aa26f9ba7cdba0a4c0919a582
David <93164111+davidcanogallardo@users.noreply.github.com>
2022-02-22 15:07:42 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# correr al clonar proyecto de github
+composer install
+# cp .env.example .env
+php artisan key:generate
+sudo ./vendor/bin/sail up -d
+sudo ./vendor/bin/sail artisan storage:link
+sudo ./vendor/bin/sail artisan key:generate
+sudo ./vendor/bin/sail artisan migrate
+
+# php artisan migrate
+# php artisan db:seed
