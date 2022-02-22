f7ec4cbb28a65e1c2a8f5c20476ad31e5e63d257
David <93164111+davidcanogallardo@users.noreply.github.com>
2022-02-22 15:08:18 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 # correr al clonar proyecto de github
 composer install
-# cp .env.example .env
+cp .env.example .env
 php artisan key:generate
-sudo ./vendor/bin/sail up -d
-sudo ./vendor/bin/sail artisan storage:link
-sudo ./vendor/bin/sail artisan key:generate
-sudo ./vendor/bin/sail artisan migrate
+#sudo ./vendor/bin/sail up -d
+#sudo ./vendor/bin/sail artisan storage:link
+#sudo ./vendor/bin/sail artisan key:generate
+#sudo ./vendor/bin/sail artisan migrate
 
 # php artisan migrate
 # php artisan db:seed
