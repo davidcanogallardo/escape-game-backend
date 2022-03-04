4ad506fc0d4c0efab9f414bd1a50b146d2e0027e
David <dcano@ibadia.cat>
2022-03-04 16:38:27 +0100

    consulta ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -11,7 +11,10 @@ use App\Models\RankingDaily;
 class RankingController extends Controller
 {
     public function getRanking() {
-        // return RankingAll::where("difficulty", "easy")->select("avgScore","user")->take(5)->with(['users:id,name'])->get()->makeHidden("user", "name");
+        // return 
+ 
+        // return DB::raw("SELECT user, avgScore AS score FROM `ranking_alls` AS owo WHERE difficulty = 'easy' ORDER BY avgScore DESC LIMIT 10; ")->get();
+        // return RankingAll::where("difficulty", "easy")->select("avgScore","user","nGames")->take(5)->with(['users:id,name'])->get()->makeHidden("user", "name");
         $levels = ["easy", "medium", "hard"];
         $ranking = [];
         foreach ($levels as $difficulty) {
@@ -27,8 +30,13 @@ class RankingController extends Controller
         
         foreach (["ranking_alls", "ranking_weeklies", "ranking_dailies"] as $table) {
             // $rank = DB::table("SELECT user, avgScore AS score FROM `$table` AS owo WHERE difficulty = '$difficulty' ORDER BY avgScore DESC LIMIT 10; ");
-            
-            $rank = RankingAll::where("difficulty", "easy")->select("avgScore AS score","user")->take(5)->with(['users:id,name'])->orderBy("avgScore", "DESC")->get()->makeHidden("user");
+            $rank = DB::table($table)
+                ->join('users', 'users.id', $table.'.user')
+                ->select($table.'.avgScore', 'users.name',$table.".user", $table.".nGames")->where($table.".difficulty", $difficulty)
+                ->orderBy("avgScore", "DESC")
+                ->take(5)
+                ->get();
+            // $rank = RankingAll::where("difficulty", "easy")->select("avgScore AS score","user")->take(5)->with(['users:id,name'])->orderBy("avgScore", "DESC")->get()->makeHidden("user");
             $rankings[$table] = $rank;
         }
         return $rankings;
